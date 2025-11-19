4d50f27fc158c43d82b828e7cad5b4995e3ecfc4
x446688 <xzp2byf924@privaterelay.appleid.com>
2025-11-19 17:06:42 +0300

    Added a little bit of error handling to autogen script

--- src/autogen.sh
+++ src/autogen.sh
@@ -1,5 +1,4 @@
 # Предполагается, что у вас уже установлены GNU Autotools. В случае, если они у вас не установлены,
 # пожалуйста, установите ;->
 
-autoreconf --install
-echo "done."
+autoreconf --install && echo "done." || echo "Error: GNU autotools is not installed. Please install them for your distribution or try using configure without this script. (Not recommended)" 
