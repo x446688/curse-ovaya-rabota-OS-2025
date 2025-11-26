5b2af8fb328275c603d874323164b037a587f327
x446688 <xzp2byf924@privaterelay.appleid.com>
2025-11-26 15:06:30 +0300

    Removed G_APPLICATION_DEFAULT_FLAGS due to not being available in Astra Linux

--- src/gui.c
+++ src/gui.c
@@ -107,7 +107,7 @@ static void app_activate (GApplication *app, gpointer *user_data) {
 }
 int main (int argc, char **argv)
 {
-	GtkApplication *app = gtk_application_new ("com.github", G_APPLICATION_DEFAULT_FLAGS);
+	GtkApplication *app = gtk_application_new ("com.github", 0); // G_APPLICATION_FLAGS_NONE
 	g_signal_connect (app, "activate", G_CALLBACK (app_activate), NULL);
 	int status = g_application_run (G_APPLICATION (app), argc, argv);
 	g_object_unref(app);
