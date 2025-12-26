f5126ee98a9210ef7c269e9717801b2a93296502
x446688 <xzp2byf924@privaterelay.appleid.com>
2025-12-27 00:07:53 +0300

    add basic testing example

--- tests/check_web.c
+++ tests/check_web.c
@@ -1,3 +1,28 @@
-int main(void){
-	return 0;
+#include <check.h>
+#include "../src/web.h"
+
+START_TEST(test_basic)
+{
+    char test[] = "test";
+    char result[] = "test";
+    remove_newlines(test);
+    ck_assert(strcmp(result, "test") == 0);
+}
+END_TEST
+
+Suite *web_suite(void) {
+    Suite *s = suite_create("Web");
+    TCase *tc = tcase_create("Basic");
+    tcase_add_test(tc, test_basic);
+    suite_add_tcase(s, tc);
+    return s;
+}
+
+int main(void) {
+    Suite *s = web_suite();
+    SRunner *sr = srunner_create(s);
+    srunner_run_all(sr, CK_NORMAL);
+    int failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (failed == 0) ? 0 : 1;
 }
